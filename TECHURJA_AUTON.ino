e48e25978935f4ac03a57b5621c186d778551a6d
shubham030 <shubham030@users.noreply.github.com>
2017-01-28 10:53:17 +0530

    Update TECHURJA_AUTON.ino

--- TECHURJA_AUTON.ino
+++ TECHURJA_AUTON.ino
@@ -83,41 +83,6 @@ ISR(PCINT0_vect){
     last_channel_3 = 0;                                 //Remember current input state
     receiver_input_channel_3 = micros() - timer_3;      //Channel 3 is micros() - timer_3
   }
-  //Channel 4=========================================
-  /*if(last_channel_4 == 0 && PINB & B00001000 ){         //Input 11 changed from 0 to 1
-    last_channel_4 = 1;                                 //Remember current input state
-    timer_4 = micros();                                 //Set timer_4 to micros()
-  }
-  else if(last_channel_4 == 1 && !(PINB & B00001000)){  //Input 11 changed from 1 to 0
-    last_channel_4 = 0;                                 //Remember current input state
-    receiver_input_channel_4 = micros() - timer_4;      //Channel 4 is micros() - timer_4
-  }
-  */
+  
 }
 
-//Subroutine for displaying the receiver signals
-/*void print_signals(){
-  Serial.print("Roll:");
-  if(receiver_input_channel_1 - 1480 < 0)Serial.print("<<<");
-  else if(receiver_input_channel_1 - 1520 > 0)Serial.print(">>>");
-  else Serial.print("-+-");
-  Serial.print(receiver_input_channel_1);
-  
-  Serial.print("  Nick:");
-  if(receiver_input_channel_2 - 1480 < 0)Serial.print("^^^");
-  else if(receiver_input_channel_2 - 1520 > 0)Serial.print("vvv");
-  else Serial.print("-+-");
-  Serial.print(receiver_input_channel_2);
-  
-  Serial.print("  Gas:");
-  if(receiver_input_channel_3 - 1480 < 0)Serial.print("vvv");
-  else if(receiver_input_channel_3 - 1520 > 0)Serial.print("^^^");
-  else Serial.print("-+-");
-  Serial.print(receiver_input_channel_3);
-  
-  Serial.print("  Yaw:");
-  if(receiver_input_channel_4 - 1480 < 0)Serial.print("<<<");
-  else if(receiver_input_channel_4 - 1520 > 0)Serial.print(">>>");
-  else Serial.print("-+-");
-  Serial.println(receiver_input_channel_4);
-}*/
